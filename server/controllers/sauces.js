cbb12e0985acf587874b0af11b99b87d288cf217
Joshua White <56774271+joshuawhite389@users.noreply.github.com>
2024-06-17 21:16:26 -0400

    added single item, update, and delete routes

--- server/controllers/sauces.js
+++ server/controllers/sauces.js
@@ -1,20 +1,31 @@
 const Sauce = require("../models/sauces");
+const fs = require("fs");
 
 exports.getSauces = (req, res, next) => {
-  Sauce.find().then(
-    (sauces) => {
+  Sauce.find()
+    .then((sauces) => {
       res.status(200).json(sauces);
-    }
-  ).catch(
-    (error) => {
+    })
+    .catch((error) => {
       res.status(400).json({
-        error: error
+        error: error,
       });
-    }
-  )
+    });
 };
 
-exports.getOneSauce = (req, res, next) => {};
+exports.getOneSauce = (req, res, next) => {
+  Sauce.findOne({
+    _id: req.params.id,
+  })
+    .then((sauce) => {
+      res.status(200).json(sauce);
+    })
+    .catch((error) => {
+      res.status(404).json({
+        error: error,
+      });
+    });
+};
 
 exports.postSauces = (req, res, next) => {
   req.body.sauce = JSON.parse(req.body.sauce);
@@ -30,23 +41,66 @@ exports.postSauces = (req, res, next) => {
     userId: req.body.sauce.userId,
   });
   console.log(sauce);
-  sauce.save().then(
-    () => {
+  sauce
+    .save()
+    .then(() => {
       res.status(201).json({
-        message: "Sauce saved successfully!"
-      })
-    }
-  ).catch(
-    (error) => {
-      res.status(400).json({
-        error: error
+        message: "Sauce saved successfully!",
       });
-    }
-  )
+    })
+    .catch((error) => {
+      res.status(400).json({
+        error: error,
+      });
+    });
 };
 
-exports.updateSauces = (req, res, next) => {};
+exports.updateSauces = (req, res, next) => {
+  const sauce = new Sauce({
+    _id: req.params.id,
+    name: req.body.name,
+    manufacturer: req.body.manufacturer,
+    description: req.body.description,
+    heat: req.body.heat,
+    imageUrl: req.body.imageUrl,
+    mainPepper: req.body.mainPepper,
+    userId: req.body.userId,
+  });
+  Sauce.updateOne({ _id: req.params.id }, sauce)
+    .then(() => {
+      res.status(201).json({
+        message: "Sauce updated successfully!",
+      });
+    })
+    .catch((error) => {
+      res.status(400).json({
+        error: error,
+      });
+    });
+};
 
-exports.deleteSauces = (req, res, next) => {};
+exports.deleteSauces = (req, res, next) => {
+  Sauce.findOne({ _id: req.params.id }).then((sauce) => {
+    const filename = sauce.imageUrl.split("/images/")[1];
+    fs.unlink("images/" + filename, () => {
+      Sauce.deleteOne({ _id: req.params.id })
+        .then(() => {
+          res.status(200).json({
+            message: "Deleted!",
+          });
+        })
+        .catch((error) => {
+          res.status(400).json({
+            error: error,
+          });
+        });
+    });
+    if (!sauce) {
+      return res.status(404).json({
+        error: new Error("No such sauce"),
+      });
+    }
+  });
+};
 
 exports.likeSauces = (req, res, next) => {};
